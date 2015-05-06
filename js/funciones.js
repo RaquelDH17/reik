62ec12577ad2eaecd8ead092769b1214332eff8f
RaquelDH17 <raquel_170298@hotmail.com>
2015-05-06 17:11:22 -0500

    Ultimo
    Modificacion final

---
+++ js/funciones.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+$(document).on("pagecreate","#contacto",function(){
+		$("#con").on("swipe",function(){
+		$("#s1").text("Podras contactarnos en raquel_170298@hotmail.com o al numero 2221073394");
+		});
+});
+
+$(document).on("pagecreate","#ayuda",function(){
+		$("#ay").on("swipe",function(){
+		$("#s2").text("Obtener ayuda!!");
+		});
+});
